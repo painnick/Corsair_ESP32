00595a7d76dbe490acf6ef28363f7217633ef133
painnick <painnick@gmail.com>
2023-08-23 00:21:59 +0900

    ADD. 격추 효과 추가

--- src/main.cpp
+++ src/main.cpp
@@ -36,6 +36,7 @@ void loop() {
         ESP_LOGI(MAIN_TAG, "Click!");
 #endif
         dfmp3.playAdvertisement(1);
+        dfmp3.loop();
 
         for (int i = 0; i < 5; i++) {
             digitalWrite(PIN_GATLING_LIGHT, HIGH);
@@ -43,7 +44,25 @@ void loop() {
             digitalWrite(PIN_GATLING_LIGHT, LOW);
             delay(100);
         }
-
         dfmp3.stopAdvertisement();
+        dfmp3.loop();
+
+#ifdef DEBUG
+        static int count = 0;
+        auto isExplosion = (++count) % 3 == 1;
+#else
+        auto isExplosion = random(0, 10) == 1;
+#endif
+        if (isExplosion) {
+#ifdef DEBUG
+            ESP_LOGI(MAIN_TAG, "Explosion!!!");
+#endif
+            delay(1000);
+            dfmp3.playAdvertisement(2);
+            dfmp3.loop();
+            delay(1400);
+            dfmp3.stopAdvertisement();
+            dfmp3.loop();
+        }
     }
 }
